8b3452c8107dee0b6287c08f3529bbb014aecd8e
mmmwembe <mmm111@hotmail.com>
2024-11-29 15:15:24 -0500

    new initial

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, send_file
+import os
+from datetime import datetime
+from modules.installed_packages import get_installed_packages
+
+app = Flask(__name__)
+app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
+app.config['SECURITY_PASSWORD_SALT'] = 'thisistheSALTforcreatingtheCONFIRMATIONtoken'
+
+app.config['DEBUG'] = True  # for development
+# app.config['DEBUG'] = False  # for production
+
+@app.route('/')
+def home():
+    # Get the installed packages
+    installed_packages = get_installed_packages()
+    
+    # Render the packages in the modules.html template
+    return render_template('modules.html', packages=installed_packages)
+
+
+@app.route('/modules')
+def modules():
+    # Get the installed packages
+    installed_packages = get_installed_packages()
+    
+    # Render the packages in the modules.html template
+    return render_template('modules.html', packages=installed_packages)
+
+
+@app.route('/download_installed_pkgs')
+def download_installed_pkgs():
+    # Get the installed packages
+    installed_packages = get_installed_packages()
+    
+    # Format the data as text
+    formatted_data = "\n".join(f"{name}: {version}" for name, version in installed_packages.items())
+    
+    # Create the filename with the current date
+    current_date = datetime.now().strftime('%m-%d-%y')
+    filename = f"Installed_Packages_{current_date}.txt"
+    file_path = os.path.join("temp_uploads", filename)
+    os.makedirs("temp_uploads", exist_ok=True)
+    
+    # Write the data to the file
+    with open(file_path, 'w') as file:
+        file.write(formatted_data)
+    
+    # Return the file for download
+    return send_file(file_path, as_attachment=True)
+
+
+if __name__ == "__main__":
+    # Use PORT from the environment, default to 8080 for local development
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
